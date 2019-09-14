f7912317f48b7726236d7d974b0a1c03f5ac1bec
邱麟 <710937671@qq.com>
2019-09-14 23:51:10 +0800

    vuex

--- vuedemo/src/components/tabbar/CartContainer.vue
+++ vuedemo/src/components/tabbar/CartContainer.vue
@@ -1,74 +1,80 @@
 <template>
-    <div>
-        <h1>购物车</h1>
-       <div class="text-center">
-    <v-dialog
-      v-model="dialog"
-      width="500"
-    >
-      <template v-slot:activator="{ on }">
-        <v-btn
-          color="red lighten-2"
-          dark
-          v-on="on"
-        >
-          Click Me
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-title
-          class="headline grey lighten-2"
-          primary-title
-        >
-          Privacy Policy
-        </v-card-title>
-
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="primary"
-            text
-            @click="dialog = false"
-          >
-            I accept
-          </v-btn>
-        </v-card-actions>
+  <div>
+              
+    <div class="productlist">
+      <v-card 
+        color="#AAAAAA"
+      
+      >
+         <v-card-text class="white--text">
+              <div class="headline mb-2">Unlimited music now</div>
+              Listen to your favorite artists and albums whenever and wherever, online and offline.
+            </v-card-text>
       </v-card>
-    </v-dialog>
-  </div>
+      <v-card 
+        color="#AAAAAA"
+      
+      >
+         <v-card-text class="white--text">
+              <div class="headline mb-2">Unlimited music now</div>
+              Listen to your favorite artists and albums whenever and wherever, online and offline.
+            </v-card-text>
+      </v-card>
+      <v-card 
+        color="#AAAAAA"
+      
+      >
+         <v-card-text class="white--text">
+              <div class="headline mb-2">Unlimited music now</div>
+              Listen to your favorite artists and albums whenever and wherever, online and offline.
+            </v-card-text>
+      </v-card>
+      <div class="checkstand">
+        <div class="selectall">
+          <input type="checkbox" name="全选" >
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 // import Vue from 'vue'
 // import Vuetify from 'vuetify'
 // import 'vuetify/dist/vuetify.min.css'
 // Vue.use(Vuetify)
- export default {
-    data () {
-      return {
-        dialog: false,
-        routename:''
-      }
-    },
-    beforeRouteEnter(to, from, next) {
-    var route = from.path.slice(1);
-    next(vm=>{
-      vm.$data.routername = route
-      console.log(vm.$data.routername);
-      
-    })
-   
+import numberbox from "../subcomponent/numberbox.vue";
+export default {
+  data() {
+    return {
+      dialog: false,
+      routename: ""
+    };
   },
-  
+  components: {
+    numberbox
+  },
+  beforeRouteEnter(to, from, next) {
+    var route = from.path.slice(1);
+    next(vm => {
+      vm.$data.routername = route;
+      console.log(vm.$data.routername);
+    });
   }
+};
 </script>
-<style scoped>
-
+<style scoped lang="scss">
+.checkstand{
+  position: fixed;
+  bottom: 56  px;
+  z-index: 10000;
+  height: 56px;
+  width: 100%;
+  background-color: saddlebrown;
+  
+  .check{
+    
+  }
+  
+  
+}
 </style>  
