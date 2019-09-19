13f78e5040c6418bebd4ade570c7309b2d581af5
邱麟 <710937671@qq.com>
2019-09-19 21:57:21 +0800

    天气页面完成

--- vuedemo/src/components/weather/weather.vue
+++ vuedemo/src/components/weather/weather.vue
@@ -1,110 +1,164 @@
 <template>
-    <div>
-        <v-app>
-            <v-card
-    class="mx-auto"
-    color="#F9F9F9"
-   width="100%"
-    min-width="320"
-  >
-    <v-list-item two-line>
-      <v-list-item-content>
-        <v-list-item-title class="headline">重庆</v-list-item-title>
-        <v-list-item-subtitle>Mon, 12:30 PM, Mostly sunny</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+  <div>
+    <v-app>
+      <v-card class="mx-auto" color="#F9F9F9" width="100%" min-width="320">
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline">{{city}}</v-list-item-title>
+            <v-list-item-subtitle
+              v-if="loading"
+            >{{forecast[index].day }}, {{updateLoc | dateFormat('HH:mm')}} {{forecast[index].txtD}}转{{forecast[index].txtN}}</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-    <v-card-text>
-      <v-row align="center">
-        <v-col class="display-3" cols="6">
-          23&deg;C
-        </v-col>
-        <v-col cols="6">
-          <v-img
-            src="https://cdn.vuetifyjs.com/images/cards/sun.png"
-            alt="Sunny image"
-            width="92"
-          ></v-img>
-        </v-col>
-      </v-row>
-    </v-card-text>
+        <v-card-text>
+          <v-row align="center">
+            <v-col class="display-3" cols="6" v-if="loading">{{weatherList[index].tmp_max}}&deg;C</v-col>
 
-    <v-list-item>
-      <v-list-item-icon>
-        <v-icon>mdi-send</v-icon>
-      </v-list-item-icon>
-      <v-list-item-subtitle>23 km/h</v-list-item-subtitle>
-    </v-list-item>
+            <v-col cols="6">
+              <v-icon size="70" color="#6e5b98" v-if="loading">{{forecast[index].icon}}</v-icon>
+            </v-col>
+          </v-row>
+        </v-card-text>
 
-    <v-list-item>
-      <v-list-item-icon>
-        <v-icon>mdi-cloud-download</v-icon>
-      </v-list-item-icon>
-      <v-list-item-subtitle>48%</v-list-item-subtitle>
-    </v-list-item>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-send</v-icon>
+          </v-list-item-icon>
+          <v-list-item-subtitle v-if="loading">{{weatherList[index].wind_spd}} km/h</v-list-item-subtitle>
+        </v-list-item>
 
-    <v-slider
-      v-model="time"
-      :max="6"
-      :tick-labels="labels"
-      class="mx-4"
-      ticks
-    ></v-slider>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-cloud-download</v-icon>
+          </v-list-item-icon>
+          <v-list-item-subtitle v-if="loading">{{weatherList[index].hum}}%</v-list-item-subtitle>
+        </v-list-item>
 
-    <v-list class="transparent">
-      <v-list-item
-        v-for="item in forecast"
-        :key="item.day"
-      >
-        <v-list-item-title>{{ item.day }}</v-list-item-title>
+        <v-slider v-model="time" :max="6" :tick-labels="labels" class="mx-4" ticks></v-slider>
 
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
+        <v-list class="transparent">
+          <v-list-item v-for="item in forecast" :key="item.day">
+            <v-list-item-title>{{ item.day }}</v-list-item-title>
 
-        <v-list-item-subtitle class="text-right">
-          {{ item.temp }}
-        </v-list-item-subtitle>
-      </v-list-item>
-    </v-list>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
 
-    <v-divider></v-divider>
+            <v-list-item-subtitle class="text-right">{{ item.temp }}</v-list-item-subtitle>
+          </v-list-item>
+        </v-list>
 
-    <v-card-actions>
-      <v-btn text @click="getWeather">Full Report</v-btn>
-    </v-card-actions>
-  </v-card>
-        </v-app>
-    </div>
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-btn text>Full Report</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-app>
+  </div>
 </template>
 <script>
 export default {
-    data () {
-      return {
-        labels: ['SU', 'MO', 'TU', 'WED', 'TH', 'FR', 'SA'],
-        time: 5,
-        forecast: [
-          { day: 'Tuesday', icon: 'mdi-white-balance-sunny', temp: '24\xB0/12\xB0' },
-          { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\xB0/14\xB0' },
-          { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' },
-        ],
-      }
-    },
-    created() {
-     //https://free-api.heweather.net/s6/weather/now?location=chongqing&key=0d347f7c994e4f8199162ed144123bd7
-    },
-    methods: {
-      getWeather(){
-        this.axios.get('https://music-api-jwzcyzizya.now.sh/api/search/song/netease?key=刘瑞琦&limit=5&page=1')
-        .then(result=>{
+  data() {
+    return {
+      labels: [],
+      time: 0,
+      forecast: [],
+      weatherList: [],
+      city: "",
+      index: 0,
+      loading: false,
+      updateLoc: ""
+    };
+  },
+  created() {
+    this.getWeather();
+    //https://free-api.heweather.net/s6/weather/now?location=chongqing&key=0d347f7c994e4f8199162ed144123bd7
+    //https://music-api-jwzcyzizya.now.sh/api/search/song/netease?key=刘瑞琦&limit=5&page=1
+  },
+  watch: {
+    time(val) {
+      console.log(this.time);
+      this.index = this.time;
+    }
+  },
+  methods: {
+    getWeather() {
+      this.axios
+        .get(
+          "https://free-api.heweather.net/s6/weather/forecast?location=chongqing&key=0d347f7c994e4f8199162ed144123bd7"
+        )
+        .then(result => {
           console.log(result);
-        })
+          console.log(result.data.HeWeather6[0].daily_forecast);
+          if (result.status === 200) {
+            console.log("ok");
+            const weekArray = new Array(
+              "星期日",
+              "星期一",
+              "星期二",
+              "星期三",
+              "星期四",
+              "星期五",
+              "星期六"
+            );
+            const weekArrayEng = new Array(
+              "Su",
+              "Mo",
+              "Tu",
+              "We",
+              "Th",
+              "Fr",
+              "Sa"
+            );
 
-      }
-    },  
-}
+            this.city = result.data.HeWeather6[0].basic.location;
+            this.updateLoc = result.data.HeWeather6[0].update.loc;
+            console.log(this.city);
+            this.weatherList = result.data.HeWeather6[0].daily_forecast;
+            for (let d in this.weatherList) {
+              this.forecast[d] = {
+                txtD: this.weatherList[d].cond_txt_d,
+                txtN: this.weatherList[d].cond_txt_n,
+                time: Number(d)
+              };
 
+              this.forecast[d].temp =
+                this.weatherList[d].tmp_max +
+                "\xB0/" +
+                this.weatherList[d].tmp_min +
+                "\xB0";
+              this.forecast[d].day =
+                weekArray[new Date(this.weatherList[d].date).getDay()];
+              // this.forecast[d]['txtN']=this.weatherList[d].cond_txt_n
+              this.labels.push(
+                weekArrayEng[new Date(this.weatherList[d].date).getDay()]
+              );
+              switch (this.forecast[d].txtD) {
+                case "晴":
+                  this.forecast[d].icon = "mdi-white-balance-sunny";
+                  break;
+                case "阴":
+                  this.forecast[d].icon = "mdi-cloud";
+                  break;
+                case "多云":
+                  this.forecast[d].icon = "mdi-weather-cloudy";
+                  break;
+                case "小雨":
+                  this.forecast[d].icon = "mdi-weather-rainy";
+                  break;
+              }
+            }
+            this.loading = true;
+            console.log(this.forecast);
+          } else {
+            console.log("error");
+          }
+        });
+    }
+  }
+};
 </script>
 <style lang="scss" scoped>
-
 </style>
